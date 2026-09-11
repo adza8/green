1f780d1bf4c7e1ef3a21e353b45fcfac02e17f93
Aditya Das <114991887+adza8@users.noreply.github.com>
2026-09-11 17:55:03 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,5 +5,14 @@ const nextConfig: NextConfig = {
     domains: ["upload.wikimedia.org", "images.unsplash.com", 'pngimg.com', 'png.pngtree.com', 'freshfruitandvegshop.com'], // allowed external image domains
   },
 };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Tells Next.js to build static HTML/CSS/JS
+  images: {
+    unoptimized: true, // Required by GitHub Pages since it doesn't support Next.js image optimization
+  },
+};
+
+export default nextConfig;
 
 export default nextConfig;
