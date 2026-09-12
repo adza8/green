2e5678515ee09afa437b39d20a7ced5152f762c7
Aditya Das <114991887+adza8@users.noreply.github.com>
2026-09-12 10:59:39 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,15 @@
 import type { NextConfig } from "next";
 
+const repoName = "green"; // ⚠️ replace with your actual GitHub repo name, case-sensitive
+
 const nextConfig: NextConfig = {
   images: {
     domains: ["upload.wikimedia.org", "images.unsplash.com", 'pngimg.com', 'png.pngtree.com', 'freshfruitandvegshop.com'],
     unoptimized: true, // Required by GitHub Pages since it doesn't support Next.js image optimization
   },
   output: 'export', // Tells Next.js to build static HTML/CSS/JS
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
   eslint: {
     ignoreDuringBuilds: true,
   },
